9955d2cc422802b3b2d68fe9f2360b7c4aafac18
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-13 17:42:19 +0200

    JSnack 1 DONE

---
+++ JSnack 1/script.js
@@ -0,0 +1,28 @@
+
+// //VERSIONE CICLO FOR
+
+// var somma= 0;
+
+// for(i=0; i<; i++){
+//    var numeroUtente = parseInt(prompt("Dimmi un numero"));
+//    somma += numeroUtente;
+// }
+
+// console.log(somma);
+
+
+//VERSIONE CICLO WHILE
+
+
+var somma= 0;
+
+var i=0;
+
+while( i< 5 ){
+   var numeroUtente=parseInt(prompt("Dimmi un numero"))
+   somma+= numeroUtente;
+   i++
+
+}
+
+console.log(somma);
